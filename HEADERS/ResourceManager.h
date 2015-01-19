a5246290d0fd038f8f30908ff0594b06b5d725f8
Alessio Linares <alessio@alessio.cc>
2015-01-19 04:26:48 +0100

    ResourceManager functional

--- HEADERS/ResourceManager.h
+++ HEADERS/ResourceManager.h
@@ -9,27 +9,85 @@ class ResourceManager
 {
 public:
 	// Constructor
-	ResourceManager();
+	ResourceManager()
+	{
+		m_resource_count = 0;
+	}
 	// Destructor
-	~ResourceManager();
+	~ResourceManager(){}
 	// Add Resource to manager
-	int addResource(const ResourceId& resource_id, const Resource& resource);
+	int addResource(const ResourceId& resource_id, const Resource& resource)
+	{
+		int ret = -1;
+		if (isLoaded(resource_id))
+		{
+			ret = m_id_table.find(resource_id)->second;
+			m_info_table.find(ret)->second.count++;
+		}
+		else
+		{
+			ret = m_resource_count++;
+			ResourceInfo ti;
+			ti.id = ret;
+			ti.count = 1;
+			ti.it = m_id_table.insert(std::pair<ResourceId, int>(resource_id, ret)).first;
+			ti.data = resource;
+			m_info_table.insert(std::pair<int, ResourceInfo>(ret, ti));
+		}
+		assert(m_id_table.size() == m_info_table.size());
+		return ret;
+	}
 	// Release a Resource resource with identifier id
-	void release(int id);
+	void release(int id)
+	{
+		ResourceInfo& ti = m_info_table.find(id)->second;
+		if(--ti.count == 0)
+		{
+			m_id_table.erase(ti.it);
+			m_info_table.erase(id);
+		}
+		assert(m_id_table.size() == m_info_table.size());
+	}
 	// Release all Resource resources with identifier id
-	void releaseAll(int id);
+	void releaseAll(int id)
+	{
+		ResourceInfo& ti = m_info_table.find(id)->second;
+		m_id_table.erase(ti.it);
+		m_info_table.erase(id);
+		assert(m_id_table.size() == m_info_table.size());
+	}
 	// Get Resource with identifier id
-	const Resource& get(int id) const;
+	const Resource& get(int id) const
+	{
+		return m_info_table.find(id)->second.data;
+	}
 	// Returns wether the Resource resource with identifier id is loaded
-	bool isLoaded(int id) const;
+	bool isLoaded(int id) const
+	{
+		return (m_info_table.find(id) != m_info_table.end());
+	}
 	// Returns wether the Resource resource with path path is loaded
-	bool isLoaded(const ResourceId& resource_id) const;
+	bool isLoaded(const ResourceId& resource_id) const
+	{
+		return (m_id_table.find(resource_id) != m_id_table.end());
+	}
 	// Returns number of active requests for resource id
-	int countResourceUsage(int id) const;
+	int countResourceUsage(int id) const
+	{
+		return m_info_table.find(id)->second.count;
+	}
 	// Returns total number of resources loaded
-	int size() const;
+	int size() const
+	{
+		return m_id_table.size();
+	}
 	// Release all resources
-	void clear();
+	void clear()
+	{
+		m_resource_count = 0;
+		m_id_table.clear();
+		m_info_table.clear();
+	}
 
 private:
 	struct ResourceInfo
--- HEADERS/TextureManager.h
+++ HEADERS/TextureManager.h
@@ -15,14 +15,37 @@ namespace detail
 class TextureManager : public ResourceManager<sf::Texture, detail::tex_id> 
 {
 public:
-	static TextureManager* instance();
+	static TextureManager* instance()
+	{
+		if (s_instance == NULL)
+			s_instance = new TextureManager();
+
+		return s_instance;
+	}
 	/* Load a Texture resource from file and return its id.
 	   Returns -1 if error while loading texture. */
-	int loadFromFile(const std::string& path, const sf::IntRect& area = sf::IntRect());
-	bool isLoaded(const std::string& path, const sf::IntRect& area = sf::IntRect()) const;
+	int loadFromFile(const std::string& path, const sf::IntRect& area = sf::IntRect())
+	{
+		sf::Texture tex;
+		tex.loadFromFile(path, area);
+		return this->addResource(makeTexId(path, area), tex);
+	}
+	bool isLoaded(const std::string& path, const sf::IntRect& area = sf::IntRect()) const
+	{
+		return ResourceManager<sf::Texture, detail::tex_id>::isLoaded(makeTexId(path, area));
+	}
 
 private:
-	static detail::tex_id makeTexId(const std::string& path, const sf::IntRect& area);
+	static detail::tex_id makeTexId(const std::string& path, const sf::IntRect& area)
+	{
+		detail::pair_int p1(area.left, area.top);
+		detail::pair_int p2(area.width, area.height);
+		detail::pair2_int p3(p1, p2);
+		detail::tex_id id(path, p3);
+		return id;
+	}
 	static TextureManager* s_instance;
 };
+
+TextureManager* TextureManager::s_instance = NULL;
 #endif
